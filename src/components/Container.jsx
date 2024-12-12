c5bd535973619198b3cd065a737632b3b0a64849
Shreyash Gavali <88925292+Shreyash-Gavali@users.noreply.github.com>
2024-12-12 17:46:16 +0000

    Commit 1.1: Changes to Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,11 @@
 import {useState,useEffect,useRef} from 'react';
 export default function Container()
 {
-    const numbers = [0,1,2,3,4,5,6,7,8,9]
-    const specialChars = ['!','@','#','$','%','^','&','*','(',')','-','_',"+","="]
+    
    let currentLength = useRef();
-   const [length,setLength] = useState(10);
+   let num = useRef();
+   let char = useRef();
+   const [length,setLength] = useState(5);
    const [charAllowed,setChar] = useState(false);
    const [numbersAllowed,setNumbers] = useState(false);
    
@@ -14,21 +15,47 @@ export default function Container()
    }
    function changeChar()
    {
-        setChar(true)
+    let val = char.current.checked;
+    setChar(val)
     console.log("Char value is"+charAllowed)
    }
    function changeNum()
    {
-            setNumbers(true)
-        console.log("Num value is"+numbersAllowed)
-    
-   }
+            let val = num.current.checked;
+            setNumbers(val)
+    }
+
     const [pass,setPass] = useState('');
    useEffect(()=>{
+    const numbers = [0,1,2,3,4,5,6,7,8,9]
+    const specialChars = ['!','@','#','$','%','^','&','*','(',')','-','_',"+","="]
+        for(let i=0;i<length;i++)
+        {
+            if(charAllowed === true && numbersAllowed === false)
+            {
+                let rand = Math.floor(Math.random()*specialChars.length);
+                let pass = []; 
+               pass = pass + specialChars[rand];
+               console.log(pass)
+                setPass(pass)
+            }
+            else if(numbersAllowed===true && charAllowed === false)
+            {
+                let rand = Math.floor(Math.random()*numbers.length);
+                let pass = ""; 
+               pass= pass + numbers[rand];
+                setPass(pass)
+                console.log("Rand is"+rand);
+                console.log(String.fromCharCode(rand))
+            }
+            else
+            {
 
+            }
+        }
     
    },[length,charAllowed,numbersAllowed])
-    console.log(currentLength)
+    // console.log(currentLength)
     return(
         <div className=" w-5/12 rounded-md h-102 mt-20 flex flex-col justify-center items-center bg-white">
             <input type="text" 
@@ -42,9 +69,8 @@ export default function Container()
             </div>
             <div className='mt-2 mb-3 flex flex-row justify-center items-center'>
                 <label className='text-xl me-5 font-[Poppins]'>Length : {length}</label>
-            <input type="range" min={0} max={20} id="length" ref={currentLength} onChange={changeLength} value={length}
-            className=' h-20 me-4 ' 
-            
+            <input type="range" min={1} max={20} id="length" ref={currentLength} onChange={changeLength} value={length}
+            className=' h-20 me-4' 
             >
             </input>
             <label htmlFor="char"
@@ -54,15 +80,14 @@ export default function Container()
             className='ms-4 h-20 w-5'
             id="char"
             onChange={changeChar}
+            ref={char}
             ></input>
             <label htmlFor="num" className='ms-4 text-xl font-[Poppins]'>Numbers</label>
-            <input type="checkbox" id="num" onChange={changeNum}
+            <input type="checkbox" id="num" onChange={changeNum} ref={num} 
             className='ms-4 h-20 w-5'
+            
             ></input>
         </div>
-        <button type="button"
-        className="bg-green-600 mt-2 mb-2 border border-slate-950 w-52 h-10 text-white rounded-2xl text-xl font-['Poppins'] border border-white"
-        >Generate</button>
         </div>
     )
 }
